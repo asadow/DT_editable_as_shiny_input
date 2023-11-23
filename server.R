99429b5ca02095e710d509f7ae9667fbd1dc73a2
adam <asadowsk@uoguelph.ca>
2023-11-23 13:42:35 -0500

    Add handsontable

--- server.R
+++ server.R
@@ -1,23 +1,23 @@
-
 library(shiny)
 library(shinyjs)
-## shinysky is to customize buttons
-library(shinysky)
 library(DT)
 library(data.table)
 library(lubridate)
 library(shinyalert)
 
 rm(list = ls())
-useShinyalert()
+
+wo <- read_rds("wo.rds")
+wider_col <- which(names(wo) == "Issue")
+
 shinyServer(function(input, output, session){
   
   ### interactive dataset 
   vals_trich<-reactiveValues()
-  vals_trich$Data<-readRDS("note.rds")
+  vals_trich$Data<-readRDS("wo.rds")
   
-  observeEvent(input$refresh, {
-    shinyjs::js$refresh()
+  observeEvent(input$refresher, {
+    shinyjs::js$refreshme()
   })
   #### MainBody_trich is the id of DT table
   output$MainBody_trich<-renderUI({
@@ -26,12 +26,24 @@ shinyServer(function(input, output, session){
           column(12,dataTableOutput("Main_table_trich"))
       ) 
     })
-  
+  output$hot <- renderRHandsontable({
+    
+  })
   #### render DataTable part ####
-  output$Main_table_trich<-renderDataTable({
-    DT=vals_trich$Data
-    datatable(DT,editable = TRUE, selection = "none") 
-    }, server = T )
+  output$Main_table_trich <- renderDataTable({
+    DT = vals_trich$Data
+    datatable(
+      DT, 
+      options = list(
+        autoWidth = TRUE,
+        columnDefs = list(
+          list(width = '600px', targets = 8 - 1)
+          )
+      ),
+      editable = TRUE, 
+      selection = "none"
+      ) 
+    }, server = TRUE)
   
   proxy = dataTableProxy('Main_table_trich')
   
@@ -46,21 +58,15 @@ shinyServer(function(input, output, session){
     
     vals_trich$Data[i, j] <<- DT::coerceValue(v, vals_trich$Data[i, j]) 
     replaceData(proxy, vals_trich$Data, resetPaging = FALSE) # important
-      
     
   })
 
-
   ### save to RDS part 
   observeEvent(input$Updated_trich,{
-    saveRDS(vals_trich$Data, "note.rds")
+    saveRDS(vals_trich$Data, "wo.rds")
     shinyalert(title = "Saved!", type = "success")
   })
   
-
-
-
- 
  ### This is nothing related to DT Editor but I think it is nice to have a download function in the Shiny so user 
  ### can download the table in csv
   output$Trich_csv<- downloadHandler(
--- ui.R
+++ ui.R
@@ -9,26 +9,24 @@
 
 library(shiny)
 library(shinyjs)
-library(shinysky)
 library(DT)
 library(data.table)
 library(lubridate)
 library(shinyalert)
-useShinyalert()
+jsCode <- "shinyjs.refreshme = function() { location.reload(); }"
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
   titlePanel("DT Editor Minimal Example"),
   shinyjs::useShinyjs(),
-  shinyjs::extendShinyjs(text = "shinyjs.refresh = function() { location.reload(); }"),
-  actionButton("refresh", "Reset",style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
+  shinyjs::extendShinyjs(text = jsCode, functions = c("refreshme")),
+  actionButton("refresher", "Reset",style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
   
   helpText("Note: Remember to save any updates!"),
   br(),
   ### tags$head() is to customize the download button
   tags$head(tags$style(".butt{background-color:#230682;} .butt{color: #e6ebef;}")),
   downloadButton("Trich_csv", "Download in CSV", class="butt"),
-  useShinyalert(), # Set up shinyalert
   uiOutput("MainBody_trich"),actionButton(inputId = "Updated_trich",label = "Save")
 ))
